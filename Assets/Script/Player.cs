3ae564ca4d78bd99e7b4bedf6fdec25293015abf
atralbus06 <atralbus@gmail.com>
2025-07-01 15:23:35 +0900

    Unity

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float speed = 5f;
+
+    Rigidbody rigid;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        float xSpeed = h * speed;
+        float zSpeed = v * speed;
+
+        Vector3 PlayerVelocity = new Vector3(xSpeed, 0f, zSpeed);
+        rigid.velocity = PlayerVelocity;
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        gameObject.SetActive(false);
+    }
+}
